dfd09292fe2b54843cfe65dc2a07bc59a32c608e
LiQingdao <Xztlqdalworld@outlook.com>
2025-06-03 16:36:16 +0800

    Update PC.v

--- PC.v
+++ PC.v
@@ -1,9 +1,12 @@
+`include "define.v"
+
 module pc(
     input clk,
     input rst,
     input jump,
     input [31:0] sext_offset,
     input zero,
+    input [2:0] control_status,
     input [31:0] jump_pc,//这里的虚实地址映射采用相等的算法，等到后期拓展了TLB之后会修正虚实地址的映射。
     output reg [31:0] pc,
     output [31:0] pc_next
@@ -21,21 +24,24 @@ module pc(
         end
         else
         begin
-            if(jump)
+            if(control_status==`IF)
             begin
-                if(zero)
-                begin
-                    pc <= branch_pc;
-                end
-                else
-                begin
-                    pc<=jump_pc;
-                end
-            end
-            else
-            begin
-                pc<=pc_next;
-            end
+             if(jump)
+              begin
+                  if(zero)
+                  begin
+                       pc <= branch_pc;
+                   end
+                  else
+                  begin
+                      pc<=jump_pc;
+                   end
+               end
+               else
+               begin
+                  pc<=pc_next;
+              end
+             end
         end
     end
 endmodule
